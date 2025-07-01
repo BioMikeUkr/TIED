b1d784194e2ae588e061e3ac178dde35b022dd99
BioMikeUkr <mykhayloshtopko@gmail.com>
2025-07-01 15:50:53 +0300

    training params optimized

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,23 +1,27 @@
 from datasets import load_dataset
+from PIL import Image
+from tqdm import tqdm
+import os
 import json
 
-dataset = load_dataset("Artificio/WikiArt", split="train")
+output_dir = "artbench"
+os.makedirs(output_dir, exist_ok=True)
 
-data = []
+dataset = load_dataset("alfredplpl/artbench-pd-256x256", split="train")
+formatted_data = []
 
-for item in dataset:
-    artist = item.get("artist", "Unknown artist")
-    title = item.get("title", "Untitled")
-    style = item.get("style", "Unknown style")
+for idx, item in tqdm(enumerate(dataset)):
+    prompt = item.get("caption", "Untitled")
 
-    prompt = f"A painting titled '{title}' in the style of {style} by {artist}."
-    image_url = item["image"]
+    image = item["image"]
+    filename = f"image_{idx+1}.jpg"
+    image_path = os.path.join(output_dir, filename)
+    image.save(image_path)
 
-    data.append({
+    formatted_data.append({
         "prompt": prompt,
-        "image": image_url
+        "image": os.path.abspath(image_path)
     })
 
 with open("wikiart_dataset.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=2)
-
+    json.dump(formatted_data, f, ensure_ascii=False, indent=2)
